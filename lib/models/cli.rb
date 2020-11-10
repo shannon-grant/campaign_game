74f193ff6eac5976c120d49f12a3348c2af05238
shannon-grant <grant.shannon90@gmail.com>
2020-11-10 17:35:04 -0500

    partial login

--- lib/models/cli.rb
+++ lib/models/cli.rb
@@ -1,5 +1,5 @@
-gem 'tty-prompt'
-
+require 'tty-prompt'
+require 'pry'
 # - login
 # - menu
 # - mulitiple choice
@@ -12,29 +12,62 @@ gem 'tty-prompt'
 # prompt.select("What size?", choices)
 # For campaign choices
 #prompt.select("Choose your campaign", %w(Scorpion Kano Jax))
+class CLI
 
-def play_game
+    @@prompt = TTY::Prompt.new
 
-    def create_a_game
-
-    end
-    
-    def game_won
-
+    def run
+        welcome
+        login_or_signup
     end
 
-    def game_lost
 
+    def play_game
+
+        def create_a_game
+
+        end
+
+        def game_won
+
+        end
+
+        def game_lost
+
+        end
+
+    end 
+
+    def welcome
+        puts "Hello! Welcome to Campaign Game!"
+        sleep(1)
     end
 
-end 
+    def login_or_signup
+        prompt = TTY::Prompt.new
+        # puts "Please enter your username to log in or sign up!"
+        # uname = gets.chomp.downcase
+        # puts "Please enter your password:"
+        # pword = gets.chomp
+        # @user = User.find_or_create_by(username: uname, password: pword, account_balance: 0)
+        # puts "Welcome #{@user.username.capitalize}!"
+        #binding.pry
+        puts "Log in or Create A New Account"
+        
+        uname = prompt.ask("Please Enter Your Username:").downcase
+        pword = prompt.mask("Please Enter Your Password:", required: true)
+        @user = User.find_or_create_by(username: uname, password: pword)
+        puts "Welcome #{@user.username.capitalize}!"
 
-def login
+        # uname = prompt.ask("Please Enter Your Username:")
+        # user = User.find_by(user: uname)
+        
+    end
 
-end
 
-def main_menu
+    def main_menu
 
+    end
 end
 
 
