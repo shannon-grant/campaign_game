96a0c40d85b9ebbc6c4bc67ac16246c118db0eaf
Ekenayy <ekenayy@gmail.com>
2020-11-11 17:19:32 -0500

    Campaign_contributions now gives total $ contributed

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -14,20 +14,25 @@ class User < ActiveRecord::Base
     end 
 
     def campaign_contributions
-        
+        #all_contributions = 0
+
         if self.my_campaigns.any? == true 
+            all_contributions = 0
             self.my_campaigns.map do |g| 
             #binding.pry
                 if g.correct == true
+                 all_contributions += g.question.reward 
                 p "You've contributed $#{g.question.reward} to #{g.charitable_campaign}"
+                sleep(2)
                 else
                 #binding.pry 
                 #p "You haven't contributed to any campaigns yet! Play a game to win money for charities."
                 #binding.pry
                 end 
             end 
+            p "Overall, you've given $#{all_contributions} to charity. You're a hero!"
         else 
-           p  "You havent played any games yet! Play a game to win money for a charity."
+           p "You havent played any games yet! Play a game to win money for a charity."
         end 
 
     end
